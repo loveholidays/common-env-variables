0150e66d6ac2e9215aebce2ea3b6ff5536d8f959
kenny fung <fungtingkwan@gmail.com>
2021-05-27 15:04:54 +0100

    log context

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import * as core from '@actions/core'
+import {context} from '@actions/github'
 
 async function run(): Promise<void> {
   try {
     core.debug(JSON.stringify(process.env, null, 2)) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+
+    core.debug(JSON.stringify(context, null, 2))
 
     const isPullRequest = !!process.env.GITHUB_HEAD_REF //GITHUB_HEAD_REF is only set for pull request events https://docs.github.com/en/actions/reference/environment-variables
 
